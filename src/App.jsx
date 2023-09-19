cc119990602d505424a182148c704dc28972c3ce
Garakoti <rahulsinghgarakoti730@gmail.com>
2023-09-19 23:54:57 +0530

    adding initial file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import loginSignUpform from './components/LoginSignUP/loginSignUPform'
+function App() {
+  
+
+  return (
+    <>
+       <div className="container">
+        <loginSignUpform/>
+       </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LoginSignUP/loginSignUPform.jsx
@@ -0,0 +1,15 @@
+import "./loginSignUPform.css"
+const loginSignUpform = () => {
+  return (
+    <div>
+      <div className="mainContainer">
+        <div className="header">
+          <h1>Login</h1>
+        </div>
+        <div className="formContainer"></div>
+      </div>
+    </div>
+  );
+};
+
+export default loginSignUpform;
